7c52cb6df85fca634b2fb3af2f8c3e9eb0e5ee15
Erin <meliaerinelizabeth@gmail.com>
2019-07-28 13:44:08 -0700

    Finished Node.h

--- Generic Dynamic Stack/Node.h
+++ Generic Dynamic Stack/Node.h
@@ -2,6 +2,23 @@
 
 template <typename T>
 class Node {
+  Node ();
+  ~Node ();
+
   T* data;
   Node* next;
 };
+
+template<typename T>
+Node<T>::Node ()
+{
+  data = nullptr;
+  next = nullptr;
+}
+
+template<typename T>
+inline Node<T>::~Node ()
+{
+  delete data;
+  delete next;
+}
