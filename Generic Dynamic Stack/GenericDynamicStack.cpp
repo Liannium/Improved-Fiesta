ddc6838cc999c4ea7cbca8284e6bbe1436a7bd20
Erin <meliaerinelizabeth@gmail.com>
2019-08-01 16:33:05 -0700

    palindrome algorithm done

--- Generic Dynamic Stack/GenericDynamicStack.cpp
+++ Generic Dynamic Stack/GenericDynamicStack.cpp
@@ -1,9 +1,44 @@
 #include "DynamicStack.h"
-#include <stdio.h>
+#include <iostream>
 
 int main()
 {
-  FILE* file;
-  DynamicStack<char> palindrome ();
+  char word[6] = "madam";
+  DynamicStack<char> palindromeForwards;
+  DynamicStack<char> palindromeBackwards;
+  int wordlength;
+  bool isPalindrome = true;
+
+  for (wordlength = 0; word[wordlength] != '\0'; wordlength++)
+    palindromeForwards.push (word[wordlength]);
+
+  for (int i = 0; i < (wordlength / 2); i++)
+  {
+    char temp;
+    palindromeForwards.pop (temp);
+    palindromeBackwards.push (temp);
+  }
+
+  if (palindromeForwards.getSize () != palindromeBackwards.getSize ())
+  {
+    char temp;
+    palindromeForwards.pop (temp);
+  }
+
+  wordlength = wordlength / 2;
+  while (isPalindrome && wordlength > 0)
+  {
+    char forward, backward;
+    palindromeForwards.pop (forward);
+    palindromeBackwards.pop (backward);
+    if (forward != backward)
+      isPalindrome = false;
+  }
+  
+  std::cout << word;
+  if (isPalindrome)
+    std::cout << " [Palindrome] \n";
+  else
+    std::cout << " [not a palindrome \n";
   return 0;
 }
