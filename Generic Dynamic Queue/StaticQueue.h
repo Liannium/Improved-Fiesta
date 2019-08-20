636b5487d7fbcc12a34a19ae4f4837881808f328
Erin <meliaerinelizabeth@gmail.com>
2019-08-20 16:23:55 -0700

    added StaticQueue.h

--- Generic Dynamic Queue/StaticQueue.h
+++ Generic Dynamic Queue/StaticQueue.h
@@ -1,7 +1,17 @@
 #include "Queue.h"
 #pragma once
 
+template <typename T>
 class StaticQueue
 {
+  public:
+    StaticQueue ();
+    ~StaticQueue ();
+    int size ();
+    bool isEmpty ();
+    void enqueue (const T& buffer);
+    T dequeue (T& buffer);
+    T peek (T& buffer);
+  private:
+    Queue<QueueElement> theQueue;
 };
-
