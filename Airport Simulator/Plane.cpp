26e1c556dfefafcd84cd3fad9c808463dec7a0f5
Erin <meliaerinelizabeth@gmail.com>
2019-08-20 17:21:12 -0700

    implemented Plane.h

--- Airport Simulator/Plane.cpp
+++ Airport Simulator/Plane.cpp
@@ -1,1 +1,46 @@
 #include "Plane.h"
+
+Plane::Plane ()
+{
+  goal = departing;
+}
+
+Plane::Plane (const int& fuel)
+{
+  fuelRemaining = fuel;
+  if (fuel == 0)
+    goal = landing;
+  else
+    goal = departing;
+}
+
+int Plane::getFuel ()
+{
+  return fuelRemaining;
+}
+
+int Plane::getWait ()
+{
+  return timeWaiting;
+}
+
+int Plane::processTurn ()
+{
+  if (goal == landing)
+  {
+    fuelRemaining--;
+    if (fuelRemaining == -1)
+      status = crashed;
+  }
+  timeWaiting++;
+}
+
+Status Plane::getStatus ()
+{
+  return status;
+}
+
+Goal Plane::getGoal ()
+{
+  return goal;
+}
--- Airport Simulator/Plane.h
+++ Airport Simulator/Plane.h
@@ -1,22 +1,22 @@
 #pragma once
 
-enum Goal {land, depart}; //taking off was two words so I had to use departing instead >.>
+enum Goal {departing, landing}; //taking off was two words so I had to use departing instead >.>
 //in C this is going to be const int landing = 0; const int departing = 1;
 enum Status {waiting, landedSafe, landedEmergency, crashed, departed};
 class Plane
 {
   public:
     Plane ();
-    Plane (int fuel);
+    Plane (const int& fuel);
     int getFuel ();
     int getWait ();
     int processTurn ();
-    void crash ();
-    void land ();
+    Status getStatus ();
+    Goal getGoal ();
 
-    Status status;
-    Goal goal;
   private:
-    int fuelRemaining;
-    int timeWaiting;
+    int fuelRemaining = 0;
+    int timeWaiting = 1;
+    Status status = waiting;
+    Goal goal;
 };
